503b9bd67555829c1f6f7a1fc0d07d6a24368a6c
gitpragyanshis <39814224+gitpragyanshis@users.noreply.github.com>
2026-04-17 00:54:17 -0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_element = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_element:
+            self.min_element.append(val)
+        else:
+            if self.min_element[-1] >= val:
+                self.min_element.append(val)
+
+    def pop(self) -> None:
+        v = self.stack.pop()
+        if self.min_element[-1] == v:
+            self.min_element.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_element[-1] if self.min_element else self.stack[-1]
